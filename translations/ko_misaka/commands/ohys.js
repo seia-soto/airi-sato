892ca943ea7a5b0509379c86d7d631af015d41c9
Seia-Soto <seia@outlook.kr>
2020-04-22 01:12:40 +0900

    Update translation

---
+++ translations/ko_misaka/commands/ohys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ohys: 'Ohys-Raws',
+  latestFeed: 'Ohys-Raws 피드',
+  currentFeedNumber: '{page}번째 페이지의 {startIndex}번째부터 5개의 항목이라고 미사카는 따뜻하게 알려줍니다.',
+  sharedWarning: '> 내부 정책에 따라 데이터는 최대 1분까지 지연될 수 있으며 데이터가 올바르지 않다고 생각하는 경우에는 help 명령어를 통해 Discord 서버에 알려달라고 미사카는 부탁합니다.',
+  fileLink: '[다운로드](https://torrents.ohys.net/t/{link})',
+  baseDescription: '최신 Ohys-Raws 피드를 불러오거나 검색하는 기능이라고 미사카는 말해줍니다.',
+  latestUsage: 'Ohys-Raws의 최근 피드를 가져오는 기능이라고 미사카는 대답합니다. 또 Ohys에서는 한 페이지에 30개의 결과를 표시하지만 현재 봇에서는 한 번에 최대 5개의 결과만을 표시한다고 미사카는 미안한 마음으로 알려줍니다.'
+}
